9c936f06700edc93c7f28b25a13a73a59a3f2320
joaquinperez94 <blogdeaula78@gmail.com>
2018-05-11 18:19:31 +0200

    Service de course

--- D12-Hackathon/Item 3/Acme-Recycling/src/main/java/services/CourseService.java
+++ D12-Hackathon/Item 3/Acme-Recycling/src/main/java/services/CourseService.java
@@ -1,11 +1,18 @@
 
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.CourseRepository;
+import domain.Course;
+import domain.Material;
+import domain.Publication;
 
 @Service
 @Transactional
@@ -14,6 +21,9 @@ public class CourseService {
 	// Managed repository -----------------------------------------------------
 	@Autowired
 	private CourseRepository	courseRepository;
+
+	@Autowired
+	private BuyerService		buyerService;
 
 
 	// Supporting services ----------------------------------------------------
@@ -24,6 +34,51 @@ public class CourseService {
 	}
 
 	// Simple CRUD methods ----------------------------------------------------
+	public Course create() {
+		Course result;
+		Collection<Publication> publications;
+		Collection<Material> materials;
+
+		publications = new ArrayList<Publication>();
+		materials = new ArrayList<Material>();
+
+		//Solo un buyer podrá crear un curso
+		Assert.isTrue(this.buyerService.checkPrincipalBoolean());
+
+		result = new Course();
+
+		result.setPublications(publications);
+		result.setMaterials(materials);
+
+		return result;
+	}
+
+	public Collection<Course> findAll() {
+		Collection<Course> result;
+		result = this.courseRepository.findAll();
+		Assert.notNull(result);
+		return result;
+	}
+
+	public Course findOne(final int courseId) {
+		Assert.isTrue(courseId != 0);
+		Course result;
+		result = this.courseRepository.findOne(courseId);
+		return result;
+	}
+
+	public Course save(final Course course) {
+		Assert.notNull(course);
+		final Course result;
+
+		//Si está en modo final la fecha es obligatoria
+		if (!course.isDraftMode())
+			Assert.notNull(course.getRealisedMoment(), "La fecha no puede ser nula");
+
+		result = this.courseRepository.save(course);
+
+		return result;
+	}
 
 	// Other business methods -------------------------------------------------
 

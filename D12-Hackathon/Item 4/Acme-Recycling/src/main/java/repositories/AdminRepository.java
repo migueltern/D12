a8ad99a4ec8cea8557c17531f8ba974665cb33f2
joaquinperez94 <blogdeaula78@gmail.com>
2018-05-18 20:36:50 +0200

    Algunas querys

--- D12-Hackathon/Item 4/Acme-Recycling/src/main/java/repositories/AdminRepository.java
+++ D12-Hackathon/Item 4/Acme-Recycling/src/main/java/repositories/AdminRepository.java
@@ -3,12 +3,15 @@ package repositories;
 
 import java.util.Collection;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Admin;
 import domain.Editor;
+import domain.LabelProduct;
 import domain.New;
 
 @Repository
@@ -25,11 +28,17 @@ public interface AdminRepository extends JpaRepository<Admin, Integer> {
 	@Query("select e from Editor e where e.news.size=(select max(n.news.size) from Editor n)")
 	Collection<Editor> findEditorsWithMoreNewsRedacted();
 
-	//QUERY III Las 5 categorías con más productos asociados
+	//QUERY III Las 5 categorías de productos con más productos asociados
+	@Query("select l from LabelProduct l where l.items.size!=0 order by l.items.size desc")
+	Page<LabelProduct> findTop5LabelProducts(Pageable pageable);
 
 	//QUERY IV La media, el mínimo, el máximo y la desviación típica de productos manejados por los manager.
+	@Query("select avg(b.courses.size),min(b.courses.size),max(b.courses.size),stddev(b.courses.size) from Buyer b")
+	Double[] avgMinMaxAndStddevOfCoursesByBuyer();
 
 	//QUERY V La media de la solicitudes con el estado Finalizadas
+	@Query("select count(i)*1.0/(select count(st) from Incidence st) from Incidence i where i.resolved=true")
+	Double avgOfIncidencesResolved();
 
 	//QUERY VI La media de usuarios que han reciclado al menos un producto.
 

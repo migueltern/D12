456c3ff4e95f4305e109e3fa1a7dc26fdc6dfe52
joaquinperez94 <blogdeaula78@gmail.com>
2018-05-20 16:35:13 +0200

    Querys optimizadas

--- D12-Hackathon/Item 4/Acme-Recycling/src/main/java/domain/Item.java
+++ D12-Hackathon/Item 4/Acme-Recycling/src/main/java/domain/Item.java
@@ -6,8 +6,10 @@ import java.util.Date;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -21,6 +23,9 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(indexes = {
+	@Index(columnList = "publicationMoment,value")
+})
 public class Item extends Opinable {
 
 	// Attributes -------------------------------------------------------------

c3a6897668224683e6770d92759d4bb326711328
MariaRuizGutierrez <marikichi1996@gmail.com>
2018-06-05 21:16:26 +0200

    feat: quitado el test funcional de editar un comentario

--- D12-Hackathon/Item 4/Acme-Recycling/src/test/java/services/CommentServiceTest.java
+++ D12-Hackathon/Item 4/Acme-Recycling/src/test/java/services/CommentServiceTest.java
@@ -70,48 +70,6 @@ public class CommentServiceTest extends AbstractTest {
 
 	}
 
-	//Edit comment
-	@Test
-	public void driveEditComment() {
-
-		final Object testingData[][] = {
-			//Editar un comentario correctante
-			{
-				"comment1", "recycler1", "editado body", null
-			}, {
-				//Editar comentario incorrectamente por el reciclar 2 que no es el suyo
-				"comment1", "recycler2", "body editado", IllegalArgumentException.class
-			}
-
-		};
-
-		for (int i = 0; i < testingData.length; i++)
-			this.templateEditComment((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (Class<?>) testingData[i][3]);
-
-	}
-	public void templateEditComment(final String entity, final String username, final String body, final Class<?> expected) {
-
-		Class<?> caught;
-		Comment comment;
-
-		caught = null;
-
-		try {
-			super.authenticate(username);
-			comment = this.commentService.findOne(super.getEntityId(entity));
-			comment.setBody(body);
-
-			this.commentService.save(comment);
-			this.commentService.flush();
-		} catch (final Throwable oops) {
-			caught = oops.getClass();
-			this.entityManager.clear();
-		}
-
-		this.checkExceptions(expected, caught);
-
-	}
-
 	//Delete comment
 	@Test
 	public void driveDeleteComment() {
